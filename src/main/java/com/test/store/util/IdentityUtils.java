cd0a423d28614b69706af66c4e3cacbfdd8866c0
Q <1592563252@qq.com>
2021-06-20 13:37:04 +0800

    feat:在IdentityUtils类中添加了 getIdentity和getUsername，并且标明了状态码的含义

--- src/main/java/com/test/store/util/IdentityUtils.java
+++ src/main/java/com/test/store/util/IdentityUtils.java
@@ -5,7 +5,12 @@ import java.util.Map;
 
 public class IdentityUtils {
 
-    final
+    public final static int NO_LOGIN = 0;
+    public final static int CONSUMER = 1;
+    public final static int EMPLOYEE = 2;
+    public final static int STOREKEEPER = 3;
+    public final static int DELIVERYMAN = 4;
+    public final static int ADMINISTRATOR = 10;
 
     public static void addIdentityMessage(HttpServletRequest request, Map<String,Object> message) {
         String username = (String) request.getSession().getAttribute("username");
@@ -13,5 +18,15 @@ public class IdentityUtils {
         message.put("username",username);
         message.put("identity",identity);
     }
+
+    public static Integer getIdentity(HttpServletRequest request) {
+        return (Integer) request.getSession().getAttribute("identity");
+    }
+
+    public static String getUsername(HttpServletRequest request) {
+        return (String) request.getSession().getAttribute("username");
+    }
+
+
 
 }
